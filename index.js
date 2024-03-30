25da3a600a24573b86497270977f36873ee0f68b
pjbrof <pjbrof@gmail.com>
2024-03-30 16:43:04 -0400

    ping pong

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("TANNYYAA");
+require("dotenv").config();
+const { Client, GatewayIntentBits } = require("discord.js");
+const cron = require("node-cron");
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+cron.schedule("* * * * *", () => {
+  console.log("TANNYYAA");
+});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!");
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
